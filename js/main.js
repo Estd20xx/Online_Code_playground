6dfbfa6b76ee82dd791575a2bd82479141c55057
sushantkhadka20xx <itsmesushant05@gmail.com>
2022-05-02 18:08:41 +0545

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let Upperdata;
+let search = () => {
+    let data = document.getElementById("data").value;
+    var Upperdata = data.toUpperCase();
+    document.getElementById("OutputScreen").innerHTML = Upperdata;
+};
+
